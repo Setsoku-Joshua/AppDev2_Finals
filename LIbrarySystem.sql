3ef0fd310d09045baf8083bc85b7392cf844d66c
Setsoku-Joshua <jorod191@gmail.com>
2025-04-19 10:04:31 +0800

    Add files via upload

---
+++ LIbrarySystem.sql
@@ -0,0 +1,167 @@
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: library_inventory
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.32-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `book_table`
+--
+
+DROP TABLE IF EXISTS `book_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `book_table` (
+  `book_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) DEFAULT NULL,
+  `author` varchar(45) DEFAULT NULL,
+  `ISBN` varchar(45) DEFAULT NULL,
+  `genre` varchar(45) DEFAULT NULL,
+  `publication_date` date DEFAULT NULL,
+  `availability_status` varchar(45) DEFAULT NULL,
+  `book_tablecol` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`book_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book_table`
+--
+
+LOCK TABLES `book_table` WRITE;
+/*!40000 ALTER TABLE `book_table` DISABLE KEYS */;
+/*!40000 ALTER TABLE `book_table` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `member_table`
+--
+
+DROP TABLE IF EXISTS `member_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `member_table` (
+  `member_id` int(11) NOT NULL AUTO_INCREMENT,
+  `member_name` varchar(45) DEFAULT NULL,
+  `contact_information` int(11) DEFAULT NULL,
+  `address` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`member_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `member_table`
+--
+
+LOCK TABLES `member_table` WRITE;
+/*!40000 ALTER TABLE `member_table` DISABLE KEYS */;
+/*!40000 ALTER TABLE `member_table` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_table`
+--
+
+DROP TABLE IF EXISTS `product_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product_table` (
+  `product_id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(45) DEFAULT NULL,
+  `description` varchar(45) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `quantity_available` int(11) DEFAULT NULL,
+  `customer_id` int(11) NOT NULL,
+  `customer_name` varchar(45) DEFAULT NULL,
+  `contact_information` bigint(13) DEFAULT NULL,
+  PRIMARY KEY (`product_id`,`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_table`
+--
+
+LOCK TABLES `product_table` WRITE;
+/*!40000 ALTER TABLE `product_table` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product_table` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `table_user`
+--
+
+DROP TABLE IF EXISTS `table_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `table_user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `fname` varchar(45) DEFAULT NULL,
+  `lname` varchar(45) DEFAULT NULL,
+  `mname` varchar(45) DEFAULT NULL,
+  `role` varchar(45) DEFAULT NULL,
+  `username` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `contact` bigint(13) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `table_user`
+--
+
+LOCK TABLES `table_user` WRITE;
+/*!40000 ALTER TABLE `table_user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `table_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tbl_logs`
+--
+
+DROP TABLE IF EXISTS `tbl_logs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbl_logs` (
+  `log_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `action` varchar(45) DEFAULT NULL,
+  `DateTime` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`log_id`),
+  KEY `log_user_id` (`user_id`),
+  CONSTRAINT `log_user_id` FOREIGN KEY (`user_id`) REFERENCES `table_user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbl_logs`
+--
+
+LOCK TABLES `tbl_logs` WRITE;
+/*!40000 ALTER TABLE `tbl_logs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tbl_logs` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-04-19 10:03:55
